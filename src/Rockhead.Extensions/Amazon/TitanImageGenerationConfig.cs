16971280a1a9b696b549c73957399ba4e87ded69
François Bouteruche <bouteru@amazon.fr>
2024-04-26 11:33:34 +0200

    Aligned TitanImageGenerationConfig to documented parameters as of the 04/26/2024 in https://docs.aws.amazon.com/bedrock/latest/userguide/model-parameters-titan-image.html

--- src/Rockhead.Extensions/Amazon/TitanImageGenerationConfig.cs
+++ src/Rockhead.Extensions/Amazon/TitanImageGenerationConfig.cs
@@ -6,41 +6,32 @@ namespace Rockhead.Extensions.Amazon
     public class TitanImageGenerationConfig
     {
         [Range(1, 5)]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyName("numberOfImages")]
-        public int NumberOfImages { get; set; }
+        public int? NumberOfImages { get; set; }
 
-        [JsonPropertyName("quality")]
-        public ImageQuality Quality { get; set; }
-
-        [Range(1, 1024)]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Range(1, 1408)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyOrder(4)]
         [JsonPropertyName("height")]
-        public int Height { get; set; }
+        public int? Height { get; set; }
 
-        [Range(1, 1024)]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Range(1, 1408)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyOrder(5)]
         [JsonPropertyName("width")]
-        public int Width { get; set; }
+        public int? Width { get; set; }
 
-        [Range(1.0, 10.0)]
-        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingDefault)]
+        [Range(1.1, 10.0)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyOrder(3)]
         [JsonPropertyName("cfgScale")]
-        public float CfgScale { get; set; }
+        public float? CfgScale { get; set; }
 
         [Range(0, 214783647)]
+        [JsonIgnore(Condition = JsonIgnoreCondition.WhenWritingNull)]
         [JsonPropertyOrder(6)]
         [JsonPropertyName("seed")]
-        public int Seed { get; set; }
-
-        [JsonConverter(typeof(JsonStringEnumConverter))]
-        public enum ImageQuality
-        {
-            Standard,
-            Premium
-        }
+        public int? Seed { get; set; }
     }
 }
